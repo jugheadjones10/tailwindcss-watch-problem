c76a089ca9c32f82ecd88c6458ccec089edd1392
root <kimyoungjin1001@gmail.com>
2022-01-07 23:56:03 +0000

    first commit

--- styles/tailwind.config.js
+++ styles/tailwind.config.js
@@ -1,12 +1,12 @@
 module.exports = {
-  content: ['_site/**/*.html'],
+  content: ["_site/**/*.html, _site/*.html"],
   safelist: [],
   theme: {
     extend: {
       colors: {
-        change: 'black',
+        change: "black",
       },
     },
   },
   plugins: [],
-}
+};
